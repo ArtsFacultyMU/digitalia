023683b75ee41b8131b4381bfcd0efe6afff4da9
Vlastimil Krejčíř <krejcir@ics.muni.cz>
2023-11-14 15:09:15 +0100

    Add update PHP support script.

---
+++ maintenance/bin/update-php.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aptitude install php8.2 php8.2-cli php8.2-common php8.2-opcache php8.2-readline php8.2-apcu php8.2-curl php8.2-dev php8.2-fpm php8.2-gd php8.2-imap php8.2-mbstring php8.2-mysql php8.2-pgsql php8.2-sqlite3 php8.2-xml php8.2-yaml
